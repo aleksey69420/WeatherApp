9009cb3b7147596ee2f159664ec15828ab7be91a
Aleksey K <aleksey.kabishau@icloud.com>
2022-06-09 22:25:00 -0400

    feat: add ui elements for current weather vc

--- WeatherApp/ViewController/Weather VC/CurrentWeatherVC.swift
+++ WeatherApp/ViewController/Weather VC/CurrentWeatherVC.swift
@@ -9,17 +9,65 @@ import UIKit
 
 final class CurrentWeatherVC: UIViewController {
 	
+	let locationLabel = UILabel()
+	let temperatureLabel = UILabel()
+	let summaryLabel = UILabel()
+	let windSpeedLabel = UILabel()
+	
+	let stackView = UIStackView()
+	
+	var now: CurrentWeatherConditions? {
+		didSet {
+			updateUI()
+		}
+	}
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		view.backgroundColor = .systemBlue
-		configure()
+		
+		configureUIElements()
+		layoutUI()
+		updateUI() // temporary remove later
 	}
 	
 	
-	private func configure() {
+	private func updateUI() {
+		locationLabel.text = "Apex"
+		temperatureLabel.text = "24℃"
+		summaryLabel.text = "Mostly Clear"
+		windSpeedLabel.text = "3 MPH"
+	}
+	
+	private func configureUIElements() {
+		view.backgroundColor = .systemBlue
+		
+		stackView.axis = .vertical
+		stackView.alignment = .center
+		stackView.distribution = .fillProportionally
+		
+	}
+	
+	private func layoutUI() {
 		view.translatesAutoresizingMaskIntoConstraints = false
 		
+		locationLabel.translatesAutoresizingMaskIntoConstraints = false
+		temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+		summaryLabel.translatesAutoresizingMaskIntoConstraints = false
+		windSpeedLabel.translatesAutoresizingMaskIntoConstraints = false
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		
+		view.addSubview(stackView)
+		
+		stackView.addArrangedSubview(locationLabel)
+		stackView.addArrangedSubview(temperatureLabel)
+		stackView.addArrangedSubview(summaryLabel)
+		stackView.addArrangedSubview(windSpeedLabel)
+		
+		NSLayoutConstraint.activate([
+			stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+		])
 	}
 }
