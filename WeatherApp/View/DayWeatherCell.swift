b517c626702191dcdbbf3ad04ae88af1a942dd7c
Aleksey K <aleksey.kabishau@icloud.com>
2022-06-15 22:59:53 -0400

    feat: add basic ui for forecast cell

--- WeatherApp/View/DayWeatherCell.swift
+++ WeatherApp/View/DayWeatherCell.swift
@@ -11,6 +11,12 @@ class DayWeatherCell: UITableViewCell {
 	
 	static let reuseId = String(describing: DayWeatherCell.self)
 	
+	let horizonalStack = UIStackView()
+	let dayLabel = UILabel()
+	let iconImageView = UIImageView()
+	let minTemperatureLabel = UILabel()
+	let maxTemperatureLabel = UILabel()
+	
 	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
 		super.init(style: style, reuseIdentifier: reuseIdentifier)
 		
@@ -21,11 +27,33 @@ class DayWeatherCell: UITableViewCell {
 	
 	
 	func configure(with day: ForecastWeatherConditions) {
-		textLabel?.text = day.summary
+		//textLabel?.text = day.summary
+		dayLabel.text = String(day.time.description)
+		iconImageView.image = UIImage(systemName: "sun.min")
+		minTemperatureLabel.text = String(day.temperatureMin)
+		maxTemperatureLabel.text = String(day.temperatureMax)
 	}
 	
 	
 	private func configure() {
+		contentView.addSubview(horizonalStack)
+		horizonalStack.translatesAutoresizingMaskIntoConstraints = false
+		horizonalStack.distribution = .fillEqually
+		horizonalStack.axis = .horizontal
+		horizonalStack.backgroundColor = .systemRed
+		
+		horizonalStack.addArrangedSubview(dayLabel)
+		horizonalStack.addArrangedSubview(iconImageView)
+		horizonalStack.addArrangedSubview(minTemperatureLabel)
+		horizonalStack.addArrangedSubview(maxTemperatureLabel)
+		
+		
+		NSLayoutConstraint.activate([
+			horizonalStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+			horizonalStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+			horizonalStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+			horizonalStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+		])
 		
 	}
 }
